d8a7c6cd6446d74a9ce50aaa9b34f144634b6de7
rachelorban <53211994+rachelorban@users.noreply.github.com>
2020-07-18 21:17:31 +0200

    test
    Pas fini juste tester github

---
+++ exercices/cheatsheet/js/script.js
@@ -0,0 +1,27 @@
+/* toute les infos pour javascript*/
+
+/* variable c'est la ou on contient les infos pour le fonctionnenent de l'apli*/
+/*let défini une variable dont la valeur est réassignable */
+let catname ="paprika";
+
+/*console log*/
+console.log(bla );
+
+/*dom*/
+document.getElementById("id")
+
+/*petit calcul*/
+
+let PremierNombre= 5;
+let DeuxiemeNombre = 9;
+console.log(PremierNombre + DeuxiemeNombre );
+
+
+/* if*/
+if(isConnected === true){
+    helloWhenConnected("Rachel");
+} else {
+    redirectTo("/login")
+};
+
+
